ebf050c237a02c7cf856ab37aefe2a1cdafdf6db
Murillo Santana <mvbentosantana@gmail.com>
2024-06-18 09:48:20 +0200

    first commit

---
+++ shd/ray-marcher.frag
@@ -0,0 +1,12 @@
+#version 460 core
+
+uniform int width;
+uniform int height;
+
+out vec4 fragment_color;
+
+void main(void)
+{
+	//fragment
+	fragment_color = vec4(0, 0, 0, 1);
+}
---
+++ src/main.cpp
@@ -0,0 +1,78 @@
+//std
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+
+//ext
+#include "external/cpp/inc/GL/glew.h"
+#include "external/cpp/inc/GL/freeglut.h"
+
+//ray-marcher
+#include "ray-marcher/inc/Scene.hpp"
+#include "ray-marcher/inc/Program.hpp"
+
+//data
+static ray_marcher::Scene* scene;
+
+//callbacks
+static void callback_idle(void)
+{
+	return;
+}
+static void callback_display(void)
+{
+	//clear
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//draw
+	scene->draw();
+	//buffers
+	glutSwapBuffers();
+}
+static void callback_reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+	glUniform1i(glGetUniformLocation(scene->program()->id(), "width"), width);
+	glUniform1i(glGetUniformLocation(scene->program()->id(), "height"), height);
+	glutPostRedisplay();
+}
+static void callback_keyboard(unsigned char key, int x1, int x2)
+{
+	if(key == 27)
+	{
+		glutDestroyWindow(glutGetWindow());
+	}
+}
+
+int main(int argc, char** argv)
+{
+	//setup
+	glutInit(&argc, argv);
+	glutInitWindowSize(900, 900);
+	glutInitWindowPosition(0, 0);
+	glutInitContextVersion(4, 6);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	//window
+	glutCreateWindow("Canvas");
+	//glew
+	if(glewInit() != GLEW_OK)
+	{
+		fprintf(stderr, "Error: can't setup glew!\n");
+		exit(EXIT_FAILURE);
+	}
+	scene = new ray_marcher::Scene;
+	//callbacks
+	glutIdleFunc(callback_idle);
+	// glutMouseFunc(callback_mouse);
+	// glutMotionFunc(callback_motion);
+	glutDisplayFunc(callback_display);
+	glutReshapeFunc(callback_reshape);
+	// glutSpecialFunc(callback_special);
+	// glutMouseWheelFunc(callback_wheel);
+	glutKeyboardFunc(callback_keyboard);
+	//start
+	glutFullScreen();
+	glutMainLoop();
+	//return
+	return EXIT_SUCCESS;
+}
